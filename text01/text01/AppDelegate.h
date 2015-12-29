dcb3cf93fe7210f53900e083baa1e921aeb277c5
yf <yf@shengxiuxiu.local>
2015-12-29 15:29:36 +0800

    test git

---
+++ text01/text01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text01
+//
+//  Created by yf on 15/12/29.
+//  Copyright © 2015年 yf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text01/text01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text01
+//
+//  Created by yf on 15/12/29.
+//  Copyright © 2015年 yf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
